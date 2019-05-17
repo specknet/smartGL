d54c3fca931dff79a25cf73a71709c0291626793
rat <rat@speckled>
2019-05-17 12:54:25 +0100

    scanning for devices

--- smartglapp/src/main/java/fr/arnaudguyon/smartglapp/MainActivity.java
+++ smartglapp/src/main/java/fr/arnaudguyon/smartglapp/MainActivity.java
@@ -16,16 +16,38 @@
 package fr.arnaudguyon.smartglapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.polidea.rxandroidble2.RxBleClient;
+import com.polidea.rxandroidble2.RxBleDevice;
+import com.polidea.rxandroidble2.scan.ScanSettings;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 import fr.arnaudguyon.smartgl.opengl.OpenGLRenderer;
 import fr.arnaudguyon.smartgl.opengl.SmartGLRenderer;
 import fr.arnaudguyon.smartgl.opengl.SmartGLView;
+import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends Activity {
+
+    private static final String ORIENT_BLE_ADDRESS = "D7:29:7E:13:28:11"; // test device
+    private static final String ORIENT_QUAT_CHARACTERISTIC = "00001526-1212-efde-1523-785feabcd125";
+
+    private RxBleDevice orient_device;
+    private Disposable scanSubscription;
+    private RxBleClient rxBleClient;
+    private ByteBuffer packetData;
+
+    private Context ctx;
+
 
     private SmartGLView mActivityGLView;
 
@@ -37,8 +59,51 @@ public class MainActivity extends Activity {
 
         mActivityGLView = (SmartGLView) findViewById(R.id.activityGLView);
         mActivityGLView.setDefaultRenderer(this);
-        mActivityGLView.setController(new GLViewController());
+        GLViewController glv = new GLViewController();
+        mActivityGLView.setController(glv);
+        ctx = this;
 
+        packetData = ByteBuffer.allocate(180);
+        packetData.order(ByteOrder.LITTLE_ENDIAN);
+
+        rxBleClient = RxBleClient.create(this);
+
+        Log.i("OrientAndroid", "calling scan");
+        scan();
+    }
+
+    private void scan() {
+        scanSubscription = rxBleClient.scanBleDevices(
+                new ScanSettings.Builder()
+                        // .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY) // change if needed
+                        // .setCallbackType(ScanSettings.CALLBACK_TYPE_ALL_MATCHES) // change if needed
+                        .build()
+                // add filters if needed
+        )
+                .subscribe(
+                        scanResult -> {
+                            Log.i("OrientAndroid", "FOUND: " + scanResult.getBleDevice().getName() + ", " +
+                                    scanResult.getBleDevice().getMacAddress());
+                            // Process scan result here.
+                            if (scanResult.getBleDevice().getMacAddress().equals(ORIENT_BLE_ADDRESS)) {
+                                runOnUiThread(() -> {
+                                    Toast.makeText(ctx, "Found " + scanResult.getBleDevice().getName() + ", " +
+                                                    scanResult.getBleDevice().getMacAddress(),
+                                            Toast.LENGTH_SHORT).show();
+                                });
+                                //connectToOrient(ORIENT_BLE_ADDRESS);
+                                scanSubscription.dispose();
+                            }
+                        },
+                        throwable -> {
+                            // Handle an error here.
+                            Log.i("OrientAndroid", "scan error");
+                            runOnUiThread(() -> {
+                                Toast.makeText(ctx, "BLE scanning error",
+                                        Toast.LENGTH_SHORT).show();
+                            });
+                        }
+                );
     }
 
     @Override
